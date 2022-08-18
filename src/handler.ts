3bf08601a7aa0bfbd3ecd659af5496bfe78e8ea4
Pierrick Turelier <pierrick@turelier.com>
2022-08-18 17:36:40 -0500

    feat(task): add Arbitrum Goerli

---
+++ src/handler.ts
@@ -0,0 +1,55 @@
+import { Relayer } from 'defender-relay-client';
+import { receiverDrawLockAndNetworkTotalSupplyPush } from '@pooltogether/v4-autotask-lib';
+import { testnet as contracts } from '@pooltogether/v4-pool-data';
+
+export async function handler(event: any) {
+  const relayer = new Relayer(event);
+  const config = {
+    beaconChain: {
+      chainId: 5,
+      providerUrl: event.secrets.ethereumGoerliProviderURL,
+    },
+    receiverChain: {
+      chainId: 421613,
+      providerUrl: event.secrets.arbitrumGoerliProviderURL,
+    },
+    allPrizePoolNetworkChains: [
+      {
+        chainId: 5,
+        providerUrl: event.secrets.ethereumGoerliProviderURL,
+      },
+      {
+        chainId: 80001,
+        providerUrl: event.secrets.polygonMumbaiProviderURL,
+      },
+      {
+        chainId: 420,
+        providerUrl: event.secrets.optimismGoerliProviderURL,
+      },
+      {
+        chainId: 421613,
+        providerUrl: event.secrets.arbitrumGoerliProviderURL,
+      },
+    ],
+  };
+
+  try {
+    const transactionPopulated = await receiverDrawLockAndNetworkTotalSupplyPush(contracts, config);
+
+    if (transactionPopulated) {
+      let transactionSentToNetwork = await relayer.sendTransaction({
+        data: transactionPopulated.data,
+        to: transactionPopulated.to,
+        gasLimit: 500000,
+        speed: 'fast',
+      });
+
+      console.log('TransactionHash:', transactionSentToNetwork.hash);
+    } else {
+      throw new Error('DrawBeacon: Transaction not populated');
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
